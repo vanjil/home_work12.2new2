ec7357e8ef2ba0289aabf8642935b5412e9adcc5
Vladimir Nikitin <vanjilbroolkyn@gmail.com>
2023-10-18 11:09:37 -0400

    fix_issue

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,9 +1,12 @@
 from utils import arrs
 
 
+from utils import arrs
+
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+    result = arrs.get([1, 2, 3], 1, "test")
+    assert result == 2, f"Expected 2, but got {result}"
+
 
 
 def test_slice():
